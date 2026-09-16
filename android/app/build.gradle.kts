c8ba30e8770e93a1945ffff2cdb77e6bd9444d6c
stiven0 <stiven2750@gmail.com>
2026-09-16 17:06:45 -0500

    update minimum android sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
         applicationId = "com.geometry_runner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
