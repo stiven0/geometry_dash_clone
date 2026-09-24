03922c79c93c700803fc73907b5c6858424a99e0
stiven0 <stiven2750@gmail.com>
2026-09-23 20:23:25 -0500

    Extend level palette options: Added additional cases for world selection in LevelSelectPage to support more diverse visual themes across levels.

--- lib/features/menu/presentation/pages/level_select_page.dart
+++ lib/features/menu/presentation/pages/level_select_page.dart
@@ -24,25 +24,46 @@ class LevelSelectPage extends ConsumerWidget {
     switch (world) {
       case 0:
         return Palettes.classic;
-
       case 1:
         return Palettes.lava;
-
       case 2:
         return Palettes.neon;
-
       case 3:
         return Palettes.ice;
-
       case 4:
         return Palettes.forest;
-
       case 5:
         return Palettes.space;
-
       case 6:
         return Palettes.desert;
-
+      case 7:
+        return Palettes.lava;
+      case 8:
+        return Palettes.neon;
+      case 9:
+        return Palettes.ice;
+      case 10:
+        return Palettes.forest;
+      case 11:
+        return Palettes.classic;
+      case 12:
+        return Palettes.lava;
+      case 13:
+        return Palettes.neon;
+      case 14:
+        return Palettes.ice;
+      case 15:
+        return Palettes.forest;
+      case 16:
+        return Palettes.space;
+      case 17:
+        return Palettes.desert;
+      case 18:
+        return Palettes.lava;
+      case 19:
+        return Palettes.neon;
+      case 20:
+        return Palettes.ice;
       default:
         return Palettes.classic;
     }
